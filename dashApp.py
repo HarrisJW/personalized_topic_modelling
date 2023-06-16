002e7e95c91854c1d05860f633c2706bc204a46c
jh <j.wilfred.harris@gmail.com>
2023-06-16 05:16:40 -0300

    Adds radio button to select model in dashApp.py

--- dashApp.py
+++ dashApp.py
@@ -7,17 +7,37 @@ import plotly.express as px
 
 app = Dash(__name__)
 
-topics = ["cats", "dogs", "trees"]
-probablities = [0.15, 0.26, 0.35]
-data = {"topics": topics, "probabilities": probablities}
+top2VecTopics = ["cats", "dogs", "trees"]
+top2VecProbablities = [0.15, 0.26, 0.35]
 
-df = pd.DataFrame(data=data)
+top2VecData = {"topics": top2VecTopics, "probabilities": top2VecProbablities}
+
+probTop2VecTopics = ["bats", "deer", "shrubs"]
+probTop2VecProbablities = [0.88, 0.55, 0.80]
+
+probTop2VecVecData = {"topics": probTop2VecTopics, "probabilities": probTop2VecProbablities}
+
+data={"probTop2Vec":  probTop2VecVecData,
+     "top2Vec": top2VecData}
+
+df = pd.DataFrame(data=data["probTop2Vec"])
 
 app.layout = html.Div([
     html.Div(children='Topic Modeling'),
+    html.Hr(),
     dash_table.DataTable(data=df.to_dict('records'), page_size=10),
-    dcc.Graph(figure=px.histogram(df, x='topics', y='probabilities'))
+    dcc.RadioItems(options=['top2Vec', 'probTop2Vec'], value='model', id='model-selector-controls'),
+    dcc.Graph(figure={},id='graph')
 ])
+
+@callback(
+    Output(component_id='graph', component_property='figure'),
+    Input(component_id='model-selector-controls', component_property='value')
+)
+    
+def update_graph(model_chosen):
+    fig = px.histogram(data[model_chosen], x='topics', y='probabilities')
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
