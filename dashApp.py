dc0856243195a7b1d3d39b78f1a4c57d889642bb
jh <j.wilfred.harris@gmail.com>
2023-06-16 04:42:57 -0300

    Adds example topic data and table to dashApp.py

--- dashApp.py
+++ dashApp.py
@@ -2,11 +2,19 @@
 #https://dash.plotly.com/tutorial
 
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import pandas as pd
 
 app = Dash(__name__)
 
+topics = ["cats", "dogs", "trees"]
+probablities = [0.15, 0.26, 0.35]
+data = {"topics": topics, "probabilities": probablities}
+
+df = pd.DataFrame(data=data)
+
 app.layout = html.Div([
-    html.Div(children='Hello World')
+    html.Div(children='Topic Modeling'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
 ])
 
 if __name__ == '__main__':
